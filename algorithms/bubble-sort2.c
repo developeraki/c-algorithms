c494494751e94b99c46ebed936d16513e5368313
akidev <106470229+developeraki@users.noreply.github.com>
2023-01-02 10:36:39 +0100

    sorting algorithms

---
+++ algorithms/bubble-sort2.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>  
+#include <assert.h>   
+#include <stdbool.h>  
+
+#define MAX 20
+
+void bubble_sort(int* array_sort)
+{
+	bool is_sorted = false;
+
+    while (!is_sorted)
+    {
+		is_sorted = true;
+
+        for (int i = 0; i < MAX - 1; i++)
+        {
+            if (array_sort[i] > array_sort[i + 1])
+            {
+
+                int change_place = array_sort[i];
+                array_sort[i] = array_sort[i + 1];
+                array_sort[i + 1] = change_place;
+
+                is_sorted = false;
+            }
+        }
+    }
+}
+
+
+static void test() {
+
+	int array_sort[MAX] = {0};
+
+    for (int i = 0; i < MAX; i++)
+    {
+        array_sort[i] = rand() % 101;
+    }
+
+
+    bubble_sort(array_sort);
+
+
+    for (int i = 0; i < MAX - 1; i++)
+    {
+        assert(array_sort[i] <= array_sort[i+1]);
+    }
+}
+
+
+int main()
+{
+    test();  
+    return 0;
+}
---
+++ algorithms/insertion-sort.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+void insertionSort(int *arr, int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i - 1;
+        int key = arr[i];
+
+        while (j >= 0 && key < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+
+static void test()
+{
+    const int size = rand() % 500; 
+    int *arr = (int *)calloc(size, sizeof(int));
+
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = (rand() % 100) - 50; 
+    }
+    insertionSort(arr, size);
+    for (int i = 0; i < size - 1; ++i)
+    {
+        assert(arr[i] <= arr[i + 1]);
+    }
+    free(arr);
+}
+int main(int argc, const char *argv[])
+{
+    srand(time(NULL));
+    test();
+    return 0;
+}
